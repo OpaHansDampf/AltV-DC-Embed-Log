0f708c3ebf7ea014f65c7a5c618be8e2bc33a432
OpaHansDampf <38101626+OpaHansDampf@users.noreply.github.com>
2022-01-27 13:29:49 +0100

    Initial commit

---
+++ Server.cs
@@ -0,0 +1,43 @@
+﻿using AltV.Net;
+using AltV.Net.Elements.Entities;
+using AltV.Net.Enums;
+using AltV.Net.ColoredConsole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dampfstube.MyPlayer;
+using System.Globalization;
+
+namespace Dampfstube
+{
+    class Server : Resource
+    {
+        public override void OnStart()
+        {
+            Alt.LogWarning("<< Server gestartet >>");
+            Utils.startstopLog("**Start: **" + (DateTime.Now.ToString("g") + " Uhr - Server gestartet!"), "Server");
+            //SQL
+            Datenbank.InitConnection();
+
+
+        }
+
+        public override void OnStop()
+        {
+            Alt.LogError(">> Server gestoppt! <<");
+           // Utils.startstopLog("**Stop: **" + (DateTime.Now.ToString("dd MMM yyyy hh:mm g") + " Uhr - Server gestoppt!"), "Admin");
+        }
+
+        public override IEntityFactory<IPlayer> GetPlayerFactory()
+        {
+            return new MyPlayerFactory();
+        }
+
+        
+
+
+
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,67 @@
+﻿using Discord;
+using Discord.Webhook;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Dampfstube
+{
+    
+    class Utils
+    {
+
+        
+        
+        public static void startstopLog(string text, string username)
+        {
+            HTTP.Post("https://discord.com/api/webhooks/935928431118520360/ecPrbHkWftaKpJFeQ0YUA2b7WgZ_qqULddHwLa7PnhQc2ahduZUFkYKcAy3wV-feglqN", new System.Collections.Specialized.NameValueCollection()
+            {
+
+                {
+                    "username",
+                    username
+                },
+                {
+                    "content",
+                    text
+                }
+            });
+        }
+
+        public static void adminLog(string text, string username)
+        {
+            HTTP.Post("https://discord.com/api/webhooks/935904519315922964/2ygxHpoqq6vims-m9lSyJ2vkLTD0zlBNnMbfXxTRBzbd_SBfypDPf2azoIzVcU_BJwsH", new System.Collections.Specialized.NameValueCollection()
+            {
+                {
+                    "username",
+                    username
+                },
+                {
+                    "content",
+                    text
+                }
+            });
+        }
+        public static void vehLog(string text, string username)
+        {
+            HTTP.Post("https://discord.com/api/webhooks/935918581382799442/WKG62XS8D1Us0CXoRhKJTZ-nY8hez4pOw68xjMFmTy_T0LLDpjVB_gQVhiIkh96-1lm6", new System.Collections.Specialized.NameValueCollection()
+            {
+                {
+                    "username",
+                    username
+                },
+                {
+                    "content",
+                    text
+                }
+            });
+        }
+
+        
+    }
+}
